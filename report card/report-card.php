8cc5316db72f9a890930d16b6c08e493d05f8612
Negar <negarkakavan@gmail.com>
2024-10-14 08:57:11 +0330

    Create Report Card Front

---
+++ report card/report-card.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./reportcard.css">
+    <title>report-card</title>
+</head>
+
+<body dir="rtl">
+    <div class="container">
+        <table class="table">
+            <tr>
+                <td class="td-one">ترم: 022</td>
+                <td class="td-one">وضعیت: عادي-ممتاز</td>
+                <td class="td-one">سنوات: 6</td>
+            </tr>
+            <tr class="tr">
+                <td class="td-two">نام درس</td>
+                <td class="td-two">تعداد واحد</td>
+                <td class="td-two">نمره</td>
+            </tr>
+            <tr>
+                <td class="td-three"></td>
+                <td class="td-three"></td>
+                <td class="td-three"></td>
+            </tr>
+            <tr>
+                <td class="td-four"></td>
+                <td class="td-four"></td>
+                <td class="td-four"></td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
