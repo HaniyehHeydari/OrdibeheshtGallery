2216b7a9a34ebb70d54a24a5d5d2ba1bfa551b7c
haniyeh <heydarihaniyeh51@gmail.com>
2024-10-26 09:31:55 +0330

    Create include

---
+++ include/includes/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./index.css">
+    <title>فروشگاه ایرانیان</title>
+</head>
+<body>
+    <div class="divTable">
+        <div class="divTableRow">
+            <div class="divTableCell">
+                <header class="divTable">
+                  <div class="divTableRow">
+                    <div class="divTableCell">لوگوی سایت<div>
+                  </div>
+                </header>
+                <nav class="divTable">
+                    <ul class="divTableRow">
+                        <li class="divTableCell">
+                            <a class="set-style-link" href="index.php">صفحه اصلی</a>
+                        </li>
+                        <li class="divTableCell">
+                            <a class="set-style-link" href="#">عضویت در سایت</a>
+                        </li>
+                        <li class="divTableCell">
+                            <a class="set-style-link" href="#">ورود به سایت</a>
+                        </li>
+                        <li class="divTableCell">
+                            <a class="set-style-link" href="#">درباره ما</a>
+                        </li>
+                        <li class="divTableCell">
+                            <a class="set-style-link" href="#">ارتباط با ما</a>
+                        </li>
+                    </ul>
+                </nav>
+                <section class="divTable">
+                   <section class="divTableRow">
+                      <aside class="divTableCell" style="width:25%;">بخش امکانات سایت</aside>
+                      <section class="divTableCell"style="width:25%;">
---
+++ include/index.php
@@ -0,0 +1,7 @@
+<?php
+include ("includes/header.php");
+?>
+محصولات
+<?php
+include ("includes/footer.php");
+?>
